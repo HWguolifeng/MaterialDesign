b3fde89b17be174b768cd9989999c24cd4abd4aa
guolifeng <glf_work@163.com>
2017-04-09 23:18:21 +0800

    1:Toolbar 的基本用法

--- app/src/main/java/com/guolifeng/android/materialdesign/MainActivity.java
+++ app/src/main/java/com/guolifeng/android/materialdesign/MainActivity.java
@@ -2,6 +2,10 @@ package com.guolifeng.android.materialdesign;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +15,35 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //调用toolbar 最关键的两句
+        Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.backup:
+                Toast.makeText(this, "yuo click BuckUp", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(this, "yuo click Delete", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.setting:
+                Toast.makeText(this, "yuo click Setting", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
